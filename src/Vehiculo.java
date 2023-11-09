b2cccea2fa3f755aa4cbb6f75934027cf7433534
Fouad <fouadaharchi1@gmail.com>
2023-11-09 17:12:09 +0100

    Creados getter and setter y añadida la foto de luchidChart

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,2 +1,75 @@
 public class Vehiculo {
+
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private String combustible;
+    private String motor;
+    private String caballos;
+    private Venta venta;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getCaballos() {
+        return caballos;
+    }
+
+    public void setCaballos(String caballos) {
+        this.caballos = caballos;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
 }
--- src/Venta.java
+++ src/Venta.java
@@ -1,2 +1,56 @@
 public class Venta {
+    private String codigoVenta;
+    private String fechaVenta;
+    private String formaPago;
+    private String precioVenta;
+    private Cliente cliente;
+    private Vehiculo vehiculo;
+
+    public String getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(String codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public String getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(String precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
 }
